4a11e0535f4d9e7cf58e0aa61d220d59e3cf8e8d
Rubén Piñeiro Fernández <rubenpifeinstituto@gmail.com>
2024-05-23 14:39:44 +0200

    modificando Json para mejor comporension

--- rick.dart
+++ rick.dart
@@ -26,6 +26,36 @@ class Rick {
     this.url,
     this.created,
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'status': status,
+        'species': species,
+        'type': type,
+        'gender': gender,
+        'origin': origin,
+        'location': location,
+        'image': image,
+        'episode': episode,
+        'url': url,
+        'created': created,
+      };
+
+  void fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    status = json['status'];
+    species = json['species'];
+    type = json['type'];
+    gender = json['gender'];
+    origin = json['origin'];
+    location = json['location'];
+    image = json['image'];
+    episode = json['episode'];
+    url = json['url'];
+    created = json['created'];
+  }
 }
 
 class Location {
